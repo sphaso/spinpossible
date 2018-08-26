62c6759b4762382e66935132aeb74b626ca9b38a
sphaso <sphaso@gmail.com>
2018-08-26 10:57:07 +0200

    completeness, better error handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,40 +1,47 @@
 module Main where
 
-import Control.Monad.State
 import System.Random
+import Control.Monad
+import Data.Char (isDigit)
+import Data.Maybe (fromJust)
 
 import Lib
 import Types
 
-stRotate :: Coord -> Coord -> State Grid ()
-stRotate c d = do
-                g <- get
-                put $ rotate c d g
+randint :: IO Int
+randint = getStdRandom $ randomR (1, 9)
 
-randint :: State StdGen Int
-randint = state $ randomR (1, 9)
-
+randomMoves :: IO [Int]
 randomMoves = replicateM 16 randint
 
-pseudoShuffle :: [a] -> [(a, a)]
-pseudoShuffle [] = []
-pseudoShuffle (a:b:c:d:xs) = (zip [a, b, c, d] xs) ++ pseudoShuffle (drop 4 xs)
+couple :: [a] -> [(a, a)]
+couple [] = []
+couple (a:b:c:d:xs) = (zip [a, b, c, d] xs) ++ couple (drop 4 xs)
+
+maybeRead :: String -> Maybe Int
+maybeRead s | all isDigit s = Just $ read s
+            | otherwise     = Nothing
 
 loop :: Grid -> IO ()
 loop  g = do
           putStrLn "Give two numbers corresponding to the upper left and bottom right corners of the submatrix"
-          a <- getLine
-          b <- getLine
-          let newg = execState (stRotate (numberToCoord $ read a) (numberToCoord $ read b)) g
-          case completed newg of
-              True  -> putStrLn "Congratulations! You win!"
-              False -> do
-                         putStrLn $ show newg
-                         loop newg
+          a <- maybeRead <$> getLine
+          b <- maybeRead <$> getLine
+          case rotate' (numberToCoord =<< a) (numberToCoord =<< b) g of
+              Left s     -> (putStrLn s >> loop g)
+              Right newg -> checkCompleteness newg
+
+checkCompleteness :: Grid -> IO ()
+checkCompleteness g | completed g = putStrLn "Congratulations! You win!"
+                    | otherwise   = (putStrLn (show g) >> loop g)
+
+rotate' :: Maybe Coord -> Maybe Coord -> Grid -> Either String Grid
+rotate' (Just a) (Just b) g = Right $ rotate a b g
+rotate' _        _        _ = Left "Input needs to be between 1 and 9"
 
 main :: IO ()
 main = do
-        let moves = pseudoShuffle $ map numberToCoord $ evalState randomMoves (mkStdGen 7) 
-        let newg = execState (mapM_ (\(a, b) -> stRotate a b) moves) pristineGrid
+        moves <- couple <$> map (fromJust . numberToCoord) <$> randomMoves
+        let newg = foldr (\(a, b) g -> rotate a b g) pristineGrid moves
         putStrLn $ show newg
         loop newg
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,19 +7,21 @@ module Lib
     ) where
 
 import Data.List (sort)
+import Data.Maybe (fromJust)
 
 import Types
 
-numberToCoord :: Int -> Coord
-numberToCoord 1 = (0, 0)
-numberToCoord 2 = (0, 1)
-numberToCoord 3 = (0, 2)
-numberToCoord 4 = (1, 0)
-numberToCoord 5 = (1, 1)
-numberToCoord 6 = (1, 2)
-numberToCoord 7 = (2, 0)
-numberToCoord 8 = (2, 1)
-numberToCoord 9 = (2, 2)
+numberToCoord :: Int -> Maybe Coord
+numberToCoord 1 = Just (0, 0)
+numberToCoord 2 = Just (0, 1)
+numberToCoord 3 = Just (0, 2)
+numberToCoord 4 = Just (1, 0)
+numberToCoord 5 = Just (1, 1)
+numberToCoord 6 = Just (1, 2)
+numberToCoord 7 = Just (2, 0)
+numberToCoord 8 = Just (2, 1)
+numberToCoord 9 = Just (2, 2)
+numberToCoord _ = Nothing
 
 coordToNumber :: Coord -> Int
 coordToNumber (a, b) = 3 * a + b + 1
@@ -37,4 +39,4 @@ revv :: Grid -> Grid
 revv xs = map (\((Tile p n s), (Tile p2 n2 s2)) -> Tile p n2 (not s2)) $ zip xs (reverse xs)
 
 pristineGrid :: Grid
-pristineGrid = [Tile (numberToCoord n) n True | n<-[1..9]]
+pristineGrid = [Tile (fromJust $ numberToCoord n) n True | n <- [1..9]]
